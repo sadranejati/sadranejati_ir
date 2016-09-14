ddf0e9ae7f14ef4b49fe4ece71be36f667d3c37f
sadra <sadranejati@gmail.com>
2016-09-14 23:25:29 +0430

    templates added and site config created

--- sadranejati_ir/settings.py
+++ sadranejati_ir/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'sadranejati_ir.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+SITE_CONFIG = {
+    'title':"یه برنامه نویس سر به هوا"
+}
